8420f43c8731f262585c290313b5a73f9e1cea0d
Farrel-Saputro <93307670+Farrel-Saputro@users.noreply.github.com>
2022-09-26 19:39:58 +0700

    Add files via upload

---
+++ UG5_71210702.py
@@ -0,0 +1,51 @@
+class Mobil:
+    _merk = ''
+    _tipe = ''
+    _jenisBahanBakar = None
+    _kapasitasBBM = None
+
+    def __init__(self,merk,tipe):
+        self._merk = merk
+        self._tipe = tipe
+    
+    def setkapasitasBBM(self,kapasitasBBM):
+        self._kapasitasBBM = kapasitasBBM
+
+    def setjenisBahanBakar(self,jenisBahanBakar):
+        self._jenisBahanBakar = jenisBahanBakar
+
+    def getmerk(self):
+        return self._merk
+
+    def gettipe(self):
+        return self._tipe
+
+    def getkapasitasBBM(self):
+        return self._kapasitasBBM
+
+    def getjenisBahanBakar(self):
+        return self._jenisBahanBakar
+
+    def printInfo(self):
+        print(f"{12*'='} INFO {12*'='}")
+        print(f"Merk            : {self.getmerk()}")
+        print(f"Tipe            : {self.gettipe()}")
+        print(f"Bahan Bakar     : {self.getjenisBahanBakar()}")
+        print(f"Kapasitas BBM   : {self.getkapasitasBBM()}")
+  
+    def isiBBM(self,harga):
+        if self.getjenisBahanBakar() is None and self.getkapasitasBBM() is None:
+            print(f"Error! Kapasitas BBM atau Jenis Bahan Bakar belum di inputkan")
+        else:
+            print(f"Mengisi {self.getkapasitasBBM()} Liter")
+            print(f"Total Harga :  {self.getkapasitasBBM() * harga}")
+
+if __name__ == "__main__":
+    mobil1 = Mobil("Toyota", "Avanza")
+    mobil1.printInfo()
+    mobil2 = Mobil("Nissan", "Grand Livina")
+    mobil2.setjenisBahanBakar("Bensin")
+    mobil2.setkapasitasBBM(20)
+    mobil2.printInfo()
+    mobil1.isiBBM(14500)
+    mobil2.isiBBM(14500)
